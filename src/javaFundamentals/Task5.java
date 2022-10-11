5d3b366252f92e8ddd7864fbb28a7a944cfca795
Vladislav <vladislavgrinev.sc@gmail.com>
2022-10-11 16:28:58 +0300

    Add tasks

--- src/javaFundamentals/Task5.java
+++ src/javaFundamentals/Task5.java
@@ -1,5 +1,41 @@
 package javaFundamentals;
 
-public class Task5 {
+import java.util.Arrays;
 
+public class Task5 {
+    public static int getCountOfRedundantMembers(int[] array) {
+
+        if (array.length < 2)
+            return 0;
+
+        int[] lengthOfSubsequence = new int[array.length];
+
+        for (int j = 1; j < array.length; j++)
+            for (int k = 0; k < j; k++)
+                if (array[j] > array[k])
+                    if (lengthOfSubsequence[j] <= lengthOfSubsequence[k])
+                        lengthOfSubsequence[j] = lengthOfSubsequence[k] + 1;
+
+        int maxLength = 0;
+        for (int length : lengthOfSubsequence)
+            if (length > maxLength)
+                maxLength = length;
+
+        return array.length - maxLength - 1;
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {};
+        int[] array2 = {1, 2, 3, 10, 4, 5, 6};
+        int[] array3 = {4, 3, 2, 1};
+        int[] array4 = {1, 2, 3, 4, 5};
+        int[] array5 = {1, 3, 2, 4, 5, 6, 8, 7, 9, 10};
+        int[] array6 = {1, 2, 2, 2, 3};
+        System.out.println(Arrays.toString(array1) + ": " + Task5.getCountOfRedundantMembers(array1));
+        System.out.println(Arrays.toString(array2) + ": " + Task5.getCountOfRedundantMembers(array2));
+        System.out.println(Arrays.toString(array3) + ": " + Task5.getCountOfRedundantMembers(array3));
+        System.out.println(Arrays.toString(array4) + ": " + Task5.getCountOfRedundantMembers(array4));
+        System.out.println(Arrays.toString(array5) + ": " + Task5.getCountOfRedundantMembers(array5));
+        System.out.println(Arrays.toString(array6) + ": " + Task5.getCountOfRedundantMembers(array6));
+    }
 }
--- src/javaFundamentals/Task8.java
+++ src/javaFundamentals/Task8.java
@@ -1,5 +1,24 @@
 package javaFundamentals;
 
 public class Task8 {
+    static void find(int[] arrayA, int[] arrayB) {
+        for (int i = 0; i < arrayA.length; i++) {
+            boolean last = true;
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i] < arrayB[j]) {
+                    System.out.printf("A[%d] before B[%d]\n", i, j);
+                    last = false;
+                    break;
+                }
+            }
+            if (last)
+                System.out.printf("A[%d] after B[%d]\n", i, arrayB.length - 1);
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] arrayA = {2, 4, 6, 8, 10};
+        int [] arrayB = {1, 3, 5, 7, 9};
+        find(arrayA, arrayB);
+    }
 }
